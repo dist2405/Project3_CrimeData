d3db6bcd353ba221c55ef29eb016fc7cd8500865
dist2405 <114367217+dist2405@users.noreply.github.com>
2022-11-21 10:29:51 -0600

    Created SQL queries for each

--- server.js
+++ server.js
@@ -11,10 +11,7 @@ let db_filename = path.join(__dirname, 'db', 'stpaul_crime.sqlite3');
 
 let app = express();
 let port = 8000;
-let SQLquery = 'SELECT case_number, date(date_time) AS date,time(date_time) as time, inc.code\
-                  , incident, incident_type, police_grid, inc.neighborhood_number, neighborhood_name,block\
-                   FROM incidents inc LEFT JOIN neighborhoods nei ON inc.neighborhood_number = nei.neighborhood_number\
-                    LEFT JOIN codes cod ON inc.code = cod.code'
+let SQLquery = '';
 
 app.use(express.json());
 
@@ -31,35 +28,52 @@ let db = new sqlite3.Database(db_filename, sqlite3.OPEN_READWRITE, (err) => {
 
 // GET request handler for crime codes
 app.get('/codes', (req, res) => {
+    SQLquery = 'SELECT code, incident_type AS type FROM codes ';
     console.log(req.query); // query object (key-value pairs after the ? in the url)
-    
+    //need if statement for certain codes
+    SQLquery = SQLquery + ' ORDER BY code ';
     res.status(200).type('json').send({}); // <-- you will need to change this
 });
 
 // GET request handler for neighborhoods
 app.get('/neighborhoods', (req, res) => {
+    SQLquery = 'SELECT neighborhood_number AS id, neighborhood_name as name FROM neighborhoods ';
     console.log(req.query); // query object (key-value pairs after the ? in the url)
-    
+    //need if statement for certain id's 
+    SQLquery = SQLquery + ' ORDER BY neighborhood_number ';
     res.status(200).type('json').send({}); // <-- you will need to change this
 });
 
 // GET request handler for crime incidents
 app.get('/incidents', (req, res) => {
+    SQLquery = 'SELECT case_number, date(date_time) AS date,time(date_time) as time,code, incident, police_grid, neighborhood_number, block FROM Incidents';
     console.log(req.query); // query object (key-value pairs after the ? in the url)
-    
+    //need if statement for start/end dates/code/grid/ neighbothood
+    SQLquery = SQLquery + 'ORDER BY date_time';
+    //need if statement here for limit
+
     res.status(200).type('json').send({}); // <-- you will need to change this
 });
-
+let SQLcheck = '';
 // PUT request handler for new crime incident
 app.put('/new-incident', (req, res) => {
     console.log(req.body); // uploaded data
+    SQLcheck = 'SELECT case_number FROM Incidents WHERE case_number = ?' ;//first we should check if this is null
+    //then insert the new values
+    SQLquery = 'INSERT INTO (case_number,date_time, code, incident,police_grid, neighborhood_number,block) VALUES (';
     
+    //once end the statement with a )
+    SQLquery = SQLquery + ')';
     res.status(200).type('txt').send('OK'); // <-- you may need to change this
 });
 
 // DELETE request handler for new crime incident
 app.delete('/remove-incident', (req, res) => {
     console.log(req.body); // uploaded data
+    SQLcheck = 'SELECT case_number FROM Incidents WHERE case_number = ?'; //first we should check if this is null
+
+    //then delete if found
+    SQLquery = 'DELETE FROM Incidents WHERE case_number = ?';
     
     res.status(200).type('txt').send('OK'); // <-- you may need to change this
 });
